1af522affcaa763066c11ec62ce1ebf22c5543b7
Yuan Zhang <bigjackCN@gmail.com>
2026-09-21 23:01:21 -0400

    feat: add Spring Boot main application class

---
+++ src/main/java/io/github/bigjackcn/musicreview/MusicReviewPlatformApplication.java
@@ -0,0 +1,11 @@
+package io.github.bigjackcn.musicreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicReviewPlatformApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MusicReviewPlatformApplication.class, args);
+    }
+}
